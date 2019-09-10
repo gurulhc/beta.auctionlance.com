361c8f95d36af58815ceb6d77681c518d51c5312
Kelvin Omereshone <kelvinomereshone@gmail.com>
2019-09-10 18:35:07 +0100

    Home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,56 +1,142 @@
 <template>
   <div>
+    <section class="main-wrapper">
+      <header id="header" class="page-header">
+        <div class="brand">
+          <h1>Auctionlance</h1>
+        </div>
+      </header>
+      <nav id="main-menu" class="menu">
+        <button id="toggle-menu" class="menu-toggle">
+          toggle menu
+        </button>
+        <section class="menu-pop-up">
+          <ul class="nav-menu">
+            <li><a href="#">Freelancers</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Blogs</a></li>
+            <li><a href="#">About</a></li>
+          </ul>
+        </section>
+      </nav>
+    </section>
+
     <nuxt />
   </div>
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
+:root {
   box-sizing: border-box;
+  font-size: calc(1vw + 0.6em);
 }
 
 *,
-*:before,
-*:after {
-  box-sizing: border-box;
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  margin: 0;
+  font-family: 'Roboto', sans-serif;
+}
+
+@media (min-width: 35em) {
+  .main-wrapper {
+    display: flex;
+  }
+}
+.page-header {
+  padding: 0.4em 1em;
+  background-color: #fff;
+}
+.menu {
+  position: relative;
+}
+
+@media (min-width: 34em) {
+  .menu {
+    width: 100%;
+  }
+}
+.menu-toggle {
+  position: absolute;
+  top: -1.15em;
+  right: 0.1em;
+  border: 0;
+  background-color: transparent;
+  font-size: 3em;
+  width: 1em;
+  height: 0.4em;
+  line-height: 0.4;
+  text-indent: 5em;
+  white-space: nowrap;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.menu-toggle::after {
+  position: absolute;
+  top: 0;
+  left: 0.2em;
+  display: block;
+  content: '\2261';
+  text-indent: 0;
+}
+
+@media (min-width: 35em) {
+  .menu-toggle {
+    display: none;
+  }
+}
+
+.menu-pop-up {
+  display: none;
+  position: absolute;
+  right: 0;
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+@media (min-width: 35em) {
+  .menu-pop-up {
+    display: block;
+    position: relative;
+    top: 2em;
+  }
+
+  .nav-menu {
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-end;
+    list-style: none;
+    margin: 0;
+    text-align: center;
+  }
+
+  .nav-menu li {
+    flex: 1;
+  }
+
+  .nav-menu li > a {
+    text-decoration: none;
+    text-transform: uppercase;
+    color: #000;
+    transition: all 300ms ease-in-out;
+    display: block;
+  }
+  .nav-menu li > a:hover {
+    background-color: #d73f2e;
+    color: #fff;
+  }
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+.menu.is-open .menu-pop-up {
+  display: block;
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.brand {
+  text-transform: lowercase;
 }
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,177 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        auctionlance.com
-      </h1>
-      <h2 class="subtitle">
-        Freelancer marketplace based off the Waves Blockchain
+  <section>
+    <main class="hero">
+      <h1 class="big">Hire easily</h1>
+      <p class="subtitle">
+        Our platform makes it easy to hire top notch
+        <span class="highlight">freelancers</span> and pay in Crypto
+      </p>
+      <a href="#" class="btn btn-primary">Hire now</a>
+    </main>
+    <section class="container about-wrapper">
+      <p class="about-auctionlance">
+        Auctionlance is a P2P freelance marketplace built on the Waves
+        blockchain. Easily find web designers, graphic designers, programmers,
+        developers, writers, editors and lots more.
+      </p>
+    </section>
+    <section class="container why">
+      <h2 class="section-heading">
+        Why Hire on <span class="highlight">Auctionlance</span>
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <div class="why-cards">
+        <div class="why-card">
+          <h3 class="caption">Built on Waves</h3>
+          <p class="content">
+            Platform built on the fastest and most secured blockchain in the
+            world.
+          </p>
+        </div>
+        <div class="why-card">
+          <h3 class="caption">AuctoNode Profit Passive Income</h3>
+          <p class="content">
+            By helping sustain the Auctionlance platform, AuctoNode owners share
+            50% profit generated on Auctionlance platform. Simply own an
+            AuctoNode and you will receive a share of platform profits in
+            proportion to the amount you hold
+          </p>
+        </div>
+        <div class="why-card">
+          <h3 class="caption">Secured Escrow</h3>
+          <p class="content">
+            Decisions are implemented based on earlier agreed terms between
+            freelancer &amp; client without interference from platform.
+          </p>
+        </div>
+        <div class="why-card">
+          <h3 class="caption">Smart Crypto Payment</h3>
+          <p class="content">
+            Payments including commissions and bonus are paid and received in
+            cryptocurrency
+          </p>
+        </div>
       </div>
-    </div>
-  </div>
+    </section>
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
   head() {
     return {
       title: 'Hire and pay freelancers with WAVES'
     }
-  },
-  components: {
-    Logo
   }
 }
 </script>
 
 <style>
+.hero,
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  padding: 0.5em 1em;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+@media (min-width: 34em) {
+  .hero,
+  .container {
+    padding: 1em 2em;
+  }
+
+  .hero {
+    padding: 1em 6em;
+  }
+}
+
+.container {
+  margin-top: 1em;
+}
+h1.big {
+  font-size: 4em;
+  margin: 0;
+}
+
+p {
+  line-height: 1.5;
 }
 
 .subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+  font-size: 1.8em;
+  margin-bottom: 2em;
 }
 
-.links {
-  padding-top: 15px;
+.about-auctionlance {
+  font-size: 1.3em;
+}
+
+@media (min-width: 34em) {
+  .about-wrapper {
+    width: 50vw;
+    margin: 0 auto;
+  }
+
+  .about-auctionlance {
+    font-size: 1em;
+  }
+}
+
+.highlight {
+  background: #d73f2e;
+  color: #fff;
+  padding: 0 0.2em;
+}
+
+a.btn {
+  text-decoration: none;
+  font-size: 1.5em;
+  padding: 0.5em 1.5em;
+}
+
+a.btn.btn-primary {
+  color: #fff;
+  background-color: #d73f2e;
+}
+
+@media (min-width: 34em) {
+  a.btn {
+    font-size: 1.2em;
+  }
+}
+.section-heading {
+  font-size: 2em;
+}
+
+h3.caption {
+  font-weight: bolder;
+  font-size: 1.5em;
+  margin-bottom: 0;
+}
+
+@media (min-width: 34em) {
+  h3.caption {
+    font-size: 1.2em;
+  }
+}
+
+.content {
+  margin-top: 1em;
+  line-height: 1.5;
+  font-size: 1em;
+}
+
+@media (min-width: 34em) {
+  .content {
+    font-size: 0.8em;
+  }
+}
+
+@media (min-width: 34em) {
+  .why {
+    padding: 2em 8em;
+  }
+  .why-cards {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 1.5em;
+  }
 }
 </style>
